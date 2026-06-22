1825167ea5600337b54becf678229d2c33a72cd7
gabe_254 <baranidigital254@gmai.com>
2026-06-22 13:31:35 +0300

    removed testimonials on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,6 @@ export default function Home() {
       <AboutSection />
       <WhyChooseUs />
       <FeaturedServices />
-      <TestimonialsSection />
       <PricingSection />
       <CTASection />
     </>
